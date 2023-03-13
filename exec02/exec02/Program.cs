6a022dca9d70f99f5f4e70f564d5487d8e97677b
skiriel <gabrieldllopes@gmail.com>
2023-03-12 21:26:44 -0300

    string arrays

--- exec02/exec02/Program.cs
+++ exec02/exec02/Program.cs
@@ -13,11 +13,21 @@ namespace exec02
             char ch = char.Parse(Console.ReadLine());
             Console.WriteLine("Qual sua altura?");
             double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Agora digite seu nome, sexo, idade e altura respectivamente");
+            string[] array = Console.ReadLine().Split(' ');
+            string nome = array[0];
+            string sexo = array[1];
+            string idade = array[2];
+            string altura = array[3];
 
             Console.WriteLine("Você digitou: ");
             Console.WriteLine(n1);
             Console.WriteLine(ch);
             Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
         }
     }
 }
