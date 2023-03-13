18db3dad016b7228a3b8841339fd1616661a230b
skiriel <gabrieldllopes@gmail.com>
2023-03-13 16:07:02 -0300

    prova03 questao01

---
+++ Prova03/Prova03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Prova03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Questão 01");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
