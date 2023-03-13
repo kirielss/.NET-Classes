23ddc4e75c07f9a35071675da71f6ecce00b18b3
skiriel <gabrieldllopes@gmail.com>
2023-03-12 22:06:22 -0300

    prova01 questao 03

--- Prova01/Prova01/Program.cs
+++ Prova01/Prova01/Program.cs
@@ -7,12 +7,23 @@ namespace Prova01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Questão 1");
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             Console.WriteLine(a + b);
 
+            Console.WriteLine("Questão 2");
             double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine((3.14159 * Math.Pow(r,2)).ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Questão 3");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+            int DIFERENCA = (A * B - C * D);
+            Console.WriteLine(DIFERENCA);
+
         }
     }
 }
