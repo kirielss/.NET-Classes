89d5e3874d415930eea706948bfcd3d1444cdfa1
skiriel <gabrieldllopes@gmail.com>
2023-03-13 10:56:32 -0300

    prova02 questao01

---
+++ Prova02/Prova02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prova02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Questão 01");
+            int num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            } else
+            {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+        }
+    }
+}
