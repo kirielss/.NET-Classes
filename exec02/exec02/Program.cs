17a8eb9ebbdbcdb27e7455ce85d37f56fd8b90e0
skiriel <gabrieldllopes@gmail.com>
2023-03-12 21:19:01 -0300

    double.parsing

---
+++ exec02/exec02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exec02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Masculino ou Feminino?");
+            char ch = char.Parse(Console.ReadLine());
+            Console.WriteLine("Qual sua altura?");
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
