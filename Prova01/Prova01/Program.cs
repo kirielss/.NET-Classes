88198d1a32d5a9990d5995577e84069f3e499fad
skiriel <gabrieldllopes@gmail.com>
2023-03-12 21:57:48 -0300

    prova questao 1

---
+++ Prova01/Prova01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Prova01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+            double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine((3.14159 * Math.Pow(r,2)).ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
