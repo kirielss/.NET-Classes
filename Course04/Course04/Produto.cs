cdcd39792b374ec89358dc130dae307d1bfc5b03
skiriel <gabrieldllopes@gmail.com>
2023-03-14 15:53:30 -0300

    my take on the OOP paradigm solution

---
+++ Course04/Course04/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course04
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int q)
+        {
+            Quantidade += q;
+            return;
+        }
+
+        public void RemoverProdutos(int q)
+        {
+            Quantidade -= q;
+            return;
+        }
+
+
+
+    }
+}
---
+++ Course04/Course04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto x = new Produto();
+            double total;
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            x.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            x.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            x.Quantidade = int.Parse(Console.ReadLine());
+
+            total = x.ValorTotalEmEstoque();
+            Console.WriteLine("Dados do produto: " + x.Nome + ", $ " + x.Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + x.Quantidade + " unidades, Total: $ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            x.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            total = x.ValorTotalEmEstoque();
+            Console.WriteLine("Dados atualizados: " + x.Nome + ", $ " + x.Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + x.Quantidade + " unidades, Total: $ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            x.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            total = x.ValorTotalEmEstoque();
+            Console.WriteLine("Dados atualizados: " + x.Nome + ", $ " + x.Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + x.Quantidade + " unidades, Total: $ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
