d85b823cc2b456ab464c0eef53757f5780de50fe
skiriel <gabrieldllopes@gmail.com>
2023-03-14 22:29:44 -0300

    feat: Revisao02 added. Just practicing OOP

---
+++ Revisao02/Revisao02/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao02
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + SalarioBruto * (porcentagem/100);
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Revisao02/Revisao02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario x = new Funcionario();
+
+            Console.Write("Nome: " );
+            x.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            x.SalarioBruto = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+            Console.Write("Imposto: ");
+            x.Imposto = double.Parse( Console.ReadLine(), CultureInfo.InvariantCulture );
+
+            Console.WriteLine("Funcionário: " + x);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            x.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados atualizados: " + x);
+        }
+    }
+}
