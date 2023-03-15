0afb161d80b2dd0860d95bf81a52c37df864f1cd
skiriel <gabrieldllopes@gmail.com>
2023-03-14 23:11:49 -0300

    feat Revisao03 - Simple program that sums test grades and tells if a student passed or not (And if not, the code shows how much he needed to pass)

---
+++ Revisao03/Revisao03/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao03 
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double N1;
+        public double N2;
+        public double N3;
+
+        public double Media()
+        {
+            return (N1 + N2 + N3);
+        }
+
+        public string AprovadoCheck()
+        {
+            if (Media() > 60)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO" + Environment.NewLine + "FALTARAM " + (60 - Media()).ToString("F2", CultureInfo.InvariantCulture) + " PONTOS";
+            }
+        }
+    }
+}
---
+++ Revisao03/Revisao03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno x = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            x.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            x.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + x.Media().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine(x.AprovadoCheck());
+        }
+    }
+}
