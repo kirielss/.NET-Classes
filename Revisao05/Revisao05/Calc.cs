6d4fb2fbd7cab94d569dffd8703df9c91646d822
skiriel <gabrieldllopes@gmail.com>
2023-03-15 10:04:38 -0300

    feat: Revisao05 - Static methods on Static Classes exercise

---
+++ Revisao05/Revisao05/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Revisao05
+{
+    internal class Calc
+    {
+        public static double Dolar(double total, double indice)
+        {
+            return (total + total * 0.06) * indice;
+        }
+    }
+}
---
+++ Revisao05/Revisao05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = " + Calc.Dolar(x,cota).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
