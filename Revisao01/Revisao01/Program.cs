96fdddd18f573c990a8df2d457f8d7797d6dce94
skiriel <gabrieldllopes@gmail.com>
2023-03-14 21:58:23 -0300

    feat: Revisao01 - utilizing override ToString to keep the main code clean while maintaining the output asked by the question

---
+++ Revisao01/Revisao01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo");
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.H = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Revisao01/Revisao01/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao01
+{
+    internal class Retangulo
+    {
+        public double B;
+        public double H;
+
+        public double Area()
+        {
+            return B * H;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (B + H);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(B, 2) + Math.Pow(H, 2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = " + Area().ToString("F2", CultureInfo.InvariantCulture) + Environment.NewLine + "PERÍMETRO = " + Perimetro().ToString("F2", CultureInfo.InvariantCulture) + Environment.NewLine + "DIAGONAL = " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
