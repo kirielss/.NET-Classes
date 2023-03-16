333089337b7e75ca4c96489a69d795bb8f23168e
skiriel <gabrieldllopes@gmail.com>
2023-03-16 14:54:06 -0300

    feat: Desafio - POO Bank simple system - working

--- Desafio/Desafio/Conta.cs
+++ Desafio/Desafio/Conta.cs
@@ -5,14 +5,17 @@ namespace Desafio
 {
     internal class Conta
     {
+        private int TAXA = 5;
+
         private string _nome;
         public int Numero { get; private set; }
         public double Saldo { get; private set; }
 
-        public Conta(string nome, int numero)
+        public Conta(string nome, int numero, double saldo)
         {
             _nome = nome;
             Numero = numero;
+            Saldo = saldo;
         }
 
         public string Nome
@@ -20,9 +23,26 @@ namespace Desafio
             get { return _nome; }
             set
             {
-                if (value != null && value.Split(' ') == string[] ) 
-                _nome = value;
+                if (value != null && value.Split(' ').Length > 1)
+                {
+                    _nome = value;
+                }
             }
+        }
+
+        public double Deposito(double entrada)
+        {
+            return Saldo += entrada;
+        }
+
+        public double Saque(double saida)
+        {
+            return Saldo -= saida - TAXA;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Nome}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
         }
     }
 }
--- Desafio/Desafio/Program.cs
+++ Desafio/Desafio/Program.cs
@@ -7,7 +7,49 @@ namespace Desafio
     {
         static void Main(string[] args)
         {
+            double inicial = -1;
             Console.Write("Entre com o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular da conta: ");
+            string pessoa = Console.ReadLine();
+
+            while (!(pessoa != null && pessoa.Split(' ').Length > 1))
+            {
+                Console.WriteLine("Atenção! Você deve digitar seu nome e sobrenome!");
+                pessoa = Console.ReadLine();
+            }
+
+            while (inicial == -1)
+            {
+                Console.Write("Haverá depósito inicial? (s/n) ");
+                char resp = char.Parse(Console.ReadLine());
+                if (resp == 's')
+                {
+                    Console.Write("Entre com o valor de depósito inicial: ");
+                    inicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+                else if (resp == 'n')
+                {
+                    inicial = 0;
+                } else
+                {
+                    Console.WriteLine("Resposta ínválida. Digite apenas um caractere 's' ou 'n'");
+                }
+            }
+
+            Conta x = new Conta(pessoa, num, inicial);
+
+            Console.WriteLine("Dados da conta: " + x);
+
+            Console.Write("Entre com um valor para depósito: ");
+            x.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados: " + Environment.NewLine + x);
+
+            Console.Write("Entre com um valor para saque: ");
+            x.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados: " + Environment.NewLine + x);
+
 
         }
     }
