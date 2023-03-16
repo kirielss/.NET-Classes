cc35bd3e24f9248725340fc81a6f02a0246fb2c3
skiriel <gabrieldllopes@gmail.com>
2023-03-16 12:20:32 -0300

    feat: Desafio - building private classes

---
+++ Desafio/Desafio/Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio
+{
+    internal class Conta
+    {
+        private string _nome;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(string nome, int numero)
+        {
+            _nome = nome;
+            Numero = numero;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Split(' ') == string[] ) 
+                _nome = value;
+            }
+        }
+    }
+}
---
+++ Desafio/Desafio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da conta: ");
+
+        }
+    }
+}
