ef6eab8ef38dd259ff1252155a1dfe0e191107ce
skiriel <gabrieldllopes@gmail.com>
2023-03-18 11:48:56 -0300

    feat: Revisao06 - Object Array exercise

---
+++ Revisao06/Revisao06/Estudante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao06
+{
+    internal class Estudante
+    {
+        public string Nome;
+        public string Email;
+        public int Quarto;
+
+        public override string ToString()
+        {
+            return $"{Quarto}: {Nome}, {Email}";
+        }
+    }
+}
---
+++ Revisao06/Revisao06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Revisao06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudante[] pensionato = new Estudante[10];
+
+            Console.Write("Digite o número de estudantes que vão alugar quartos: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite o nome do estudante: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o email do estudante: ");
+                string email = Console.ReadLine();
+                Console.Write("Digite o número do quarto desejado entre 1 e 10: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                pensionato[i] = new Estudante { Nome = nome, Email = email, Quarto = quarto };
+
+            }
+
+            for (int i = 0; i < pensionato.Length; i++)
+            {
+                Console.WriteLine(pensionato[i]);
+            }
+        }
+    }
+}
