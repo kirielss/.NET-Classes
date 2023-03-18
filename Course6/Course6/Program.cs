dc6c4d05d6f2748575088451db03e101feb96f14
skiriel <gabrieldllopes@gmail.com>
2023-03-18 14:56:55 -0300

    feat: Course6 - params classes

---
+++ Course6/Course6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = Calculator.Sum(10, 20, 30, 40, 50);
+
+            Console.WriteLine(soma);
+        }
+    }
+}
