8715d2df63a909f1deb7ed840c7f7eff8ef8299f
skiriel <gabrieldllopes@gmail.com>
2023-03-19 21:59:00 -0300

    aOK

---
+++ Course7/Course7/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course7
+{
+    class Calculator
+    {
+        public static void Triple(int x)
+        {
+            x = x * 3;
+        }
+    }
+}
---
+++ Course7/Course7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(a);
+            Console.WriteLine(a);
+        }
+    }
+}
