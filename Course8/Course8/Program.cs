c80417e4e5c1547b9d247853d197faa4156041bd
skiriel <gabrieldllopes@gmail.com>
2023-03-20 23:50:17 -0300

    feat: Course8 - foreach

---
+++ Course8/Course8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Course8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[]
+            {
+                "Maria",
+                "Joao",
+                "Francisco"
+            };
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            foreach (string nome in vect)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
